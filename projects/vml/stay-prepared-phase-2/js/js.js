58536d3fdbe2cd86ccc6a70e44f5ccc1b5df06a6
developer-murtaza <developer.murtaza@gmail.com>
2021-01-28 10:13:51 +0530

    stay prepared phase 2 mobile banners

--- projects/vml/stay-prepared-phase-2/js/js.js
+++ projects/vml/stay-prepared-phase-2/js/js.js
@@ -1,30 +1,30 @@
 $(function() {
     /* expand-collapse of FAQ's starts */
-    $(".faq-wrapper > .row > .question").on("click", function() {
-        //$(this).parent(".row").siblings(".row").children(".question").removeClass("toggle-open");
-        //$(this).parent(".row").siblings(".row").children(".answer").slideUp();
-        $(this).toggleClass("toggle-open");
-        $(this).next(".answer").slideToggle();
+    // $(".faq-wrapper > .row > .question").on("click", function() {
+    //     //$(this).parent(".row").siblings(".row").children(".question").removeClass("toggle-open");
+    //     //$(this).parent(".row").siblings(".row").children(".answer").slideUp();
+    //     $(this).toggleClass("toggle-open");
+    //     $(this).next(".answer").slideToggle();
 
-        if($(this).parent(".row").siblings(".toggle-expand").find("span[data-rel=collapse-all]").is(":visible")) {
-            $(this).parent(".row").siblings(".toggle-expand").removeClass("toggle-open");
-        }
-    });
+    //     if($(this).parent(".row").siblings(".toggle-expand").find("span[data-rel=collapse-all]").is(":visible")) {
+    //         $(this).parent(".row").siblings(".toggle-expand").removeClass("toggle-open");
+    //     }
+    // });
 
-    $(".faq-wrapper > .row.toggle-expand a").on("click", function() {
-        if($(this).children("span[data-rel=expand-all]").is(":visible")) {
-            $(this).parent().siblings(".row").children(".question").addClass("toggle-open");
-            $(this).parent().siblings(".row").children(".question").next(".answer").slideDown();
-        }
-        else
-        {
-            $(this).parent().siblings(".row").children(".question").removeClass("toggle-open");
-            $(this).parent().siblings(".row").children(".question").next(".answer").slideUp();
-        }
+    // $(".faq-wrapper > .row.toggle-expand a").on("click", function() {
+    //     if($(this).children("span[data-rel=expand-all]").is(":visible")) {
+    //         $(this).parent().siblings(".row").children(".question").addClass("toggle-open");
+    //         $(this).parent().siblings(".row").children(".question").next(".answer").slideDown();
+    //     }
+    //     else
+    //     {
+    //         $(this).parent().siblings(".row").children(".question").removeClass("toggle-open");
+    //         $(this).parent().siblings(".row").children(".question").next(".answer").slideUp();
+    //     }
         
-        $(this).parent().toggleClass("toggle-open");
+    //     $(this).parent().toggleClass("toggle-open");
         
-    });
+    // });
     /* expand-collapse of FAQ's ends */
 
     if($(".banner-section").length) {
@@ -41,15 +41,22 @@ $(function() {
         }
 
         $(".banner-section .carousel-content").each(function() {
-            $(this).css("background-image","url('" + $(this).attr('data-img-src') + "')");
+            if($(window).width() >= 768) {
+                $(this).css("background-image","url('" + $(this).attr('data-img-src') + "')");
+            }
+            else {
+                $(this).css("background-image","url('" + $(this).attr('data-img-src-mobile') + "')");
+            }
+            
+            
         }).promise().done(function() {
             $(".banner-section.owl-carousel").owlCarousel({
                 dots: true,
                 nav: false,
                 loop: true,
-                //autoplay: true,
-                //autoplayTimeout: 5000,
-                //autoplayHoverPause: true,
+                autoplay: true,
+                autoplayTimeout: 5000,
+                autoplayHoverPause: true,
                 center: true,
                 responsive: {
                     0: {
