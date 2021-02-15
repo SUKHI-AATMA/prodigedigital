486332e584050ffd737f7a6b70e10fdee231239c
saifnagauri <pravin.auti@liqvd.asia>
2021-02-15 20:44:56 +0530

    mestermor quiz page

---
+++ projects/mestermor/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    //====== quiz page - start
+    
+    $('.quiz-gallery-container').fadeIn(5000);
+
+    $('#chartClick1').on("click", function() {
+        $('#chartPopup1').toggleClass("active");
+    });
+    $('#chartClick2').on("click", function() {
+        $('#chartPopup2').toggleClass("active");
+    });
+    
+    //====== quiz page - end
+
+
+
+});
